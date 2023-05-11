71f4ad3df66551bdd7ef23857b1c5b04d7ae9453
esavitz <ezrasavitz@gmail.com>
2023-05-11 16:45:22 -0400

    fixed attributes

--- backend/model.py
+++ backend/model.py
@@ -2,24 +2,32 @@ import osmnx as ox
 import networkx as nx
 
 class MapGraph:
-    def __init__(self, location):
+    def __init__(self, locations):
         """
         Initialize a MapGraph object.
 
         Args:
-            location (str): Name of the location to retrieve the graph for.
+            locations (list): Names of the start and end locations to retrieve the graph for.
 
         Attributes:
-            location (str): Name of the location.
+            locations (list): Names of the start and end locations.
             graph (networkx.Graph): Networkx graph representing the street network.
             nodes (list): List of nodes in the graph.
             edges (list): List of edges in the graph.
             shortest_path (list): List of nodes in the shortest path.
+            shortest_path_length (float): Length of the shortest path in meters.
+            shortest_path_elevation (float): Elevation gain of the shortest path in meters.
             result_path (list): List of nodes in the result path as requested by the user.
+            result_path_length (float): Length of the result path in meters.
+            result_path_elevation (float): Elevation gain of the result path in meters.
         """
-        self.location = location
-        self.graph = ox.graph_from_place(location, network_type='all')
+        self.location = locations
+        self.graph = ox.graph_from_place(locations, network_type='all')
         self.nodes = self.graph.nodes()
         self.edges = self.graph.edges()
         self.shortest_path = None
+        self.shortest_path_length = None
+        self.shortest_path_elevation = None
         self.result_path = None
+        self.result_path_length = None
+        self.result_path_elevation = None
