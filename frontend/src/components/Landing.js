d148bb383296ceed32d7473253553d8cde5596c8
esavitz <ezrasavitz@gmail.com>
2023-05-16 15:39:14 -0400

    map + route rendering in frontend!

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './styles/Landing.css';
+import MapBox from './MapBox.js'
+import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
 
 function Landing() {
   const [route, setRoute] = useState(null); // [route, setRoute]
@@ -9,8 +14,17 @@ function Landing() {
   const [percentage, setPercentage] = useState(1);
   const [max, setMax] = useState(true);
 
+  // useEffect(() => {
+  //   if(route) {
+  //     console.log(route);
+
   const getRoute = async () => {
-    const res = await axios.get('http://localhost:5000/get_route');
+    const res = await axios.post('http://localhost:5000/get_route',{
+      "start": start,
+      "end": end,
+      "percentage": percentage,
+      "max": max
+    });
     setRoute(res.data);
     console.log(res);
   };
@@ -35,14 +49,11 @@ function Landing() {
     console.log(max)
   };
 
+  
+
   return (
-    <div>
-      <div className='welcome-box'>
-        <h1>Welcome to EleNa</h1>
-        <button onClick={getRoute}>Get Started</button>
-        <p>{route}</p>
-      </div>
-      <div className='left-box'>
+    <div className='landing-container'>
+      <div className='options-box'>
         <h1 className='title'>EleNa</h1>
         <h1>Start Location</h1>
         <form>
@@ -67,6 +78,26 @@ function Landing() {
           <button onClick={handleMax}>{max ? 'MIN' : 'MAX'}</button>
           <p>Toggle state: {max ? 'MIN' : 'MAX'}</p>
         </div>
+        <button onClick={getRoute}>Submit</button>
+      </div>
+      <div className='map-box'>
+        <MapContainer
+        center={[42.374821, -72.518915]}
+        zoom={13}
+        style={{ width: '100%', height: '100%' }}
+        >
+        <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="Map data © OpenStreetMap contributors"
+        />
+        {route && (
+            <Polyline
+              positions={route}
+              color="red"
+              weight={3}
+            />
+          )}
+        </MapContainer>
       </div>
     </div>
   );
