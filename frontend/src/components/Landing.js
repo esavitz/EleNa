b2d614617e295d0287f215a2aa5b8f5bb5d5095d
ajnguy <ajnguy@umass.edu>
2023-05-15 21:24:48 -0400

    added left box and react app variables

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,9 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './styles/Landing.css';
 
 function Landing() {
   const [route, setRoute] = useState(null); // [route, setRoute]
+  const [startPosition, setStartPosition] = useState("");
+  const [destination, setDestination] = useState("");
+  const [multiplier, setMultiplier] = useState(.5);
+  const [routeType, setRouteType] = useState(true);
 
   const getRoute = async () => {
     const res = await axios.get('http://localhost:5000/get_route');
@@ -12,10 +16,15 @@ function Landing() {
   };
 
   return (
-    <div className='welcome-box'>
-      <h1>Welcome to EleNa</h1>
-      <button onClick={getRoute}>Get Started</button>
-      <p>{route}</p>
+    <div>
+      <div className='welcome-box'>
+        <h1>Welcome to EleNa</h1>
+        <button onClick={getRoute}>Get Started</button>
+        <p>{route}</p>
+      </div>
+      <div className='left-box'>
+        
+      </div>
     </div>
   );
 }
