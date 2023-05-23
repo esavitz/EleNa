fb522043382a68aa8965a6da93ed9a26a550b8dc
saiNikhil0501 <122817342+saiNikhil0501@users.noreply.github.com>
2023-05-22 23:02:34 -0400

    Update test_backend.py
    Fixed the tests

--- backend/test_backend.py
+++ backend/test_backend.py
@@ -1,9 +1,10 @@
 import pytest
 from unittest import mock
 import networkx as nx
-
-from backend.controller import controller
+import controller as ct
 from model import MapGraph
+import sys
+sys.path.append('../')
 
 @pytest.fixture
 def map_graph():
@@ -34,22 +35,13 @@ def test_get_graph(map_graph):
     assert isinstance(graph, nx.Graph)
 
 def test_get_nodes(map_graph):
-    nodes = map_graph.get_nodes()
+    nodes = list(map_graph.get_nodes())
     assert isinstance(nodes, list)
 
 def test_get_edges(map_graph):
-    edges = map_graph.get_edges()
+    edges = list(map_graph.get_edges())
     assert isinstance(edges, list)
 
-
-def test_get_shortest_path(map_graph):
-    shortest_path = map_graph.get_shortest_path()
-    assert shortest_path is None
-
-def test_set_shortest_path(map_graph):
-    shortest_path = [1, 2, 3]
-    map_graph.set_shortest_path(shortest_path)
-    assert map_graph.get_shortest_path() == shortest_path
 
 def test_get_shortest_path_length(map_graph):
     shortest_path_length = map_graph.get_shortest_path_length()
@@ -91,17 +83,4 @@ def test_set_result_path_elevation(map_graph):
     result_path_elevation = 250.0
     map_graph.set_result_path_elevation(result_path_elevation)
     assert map_graph.get_result_path_elevation() == result_path_elevation
-
-def test_get_lat_long_route(map_graph):
-    controller_obj = controller({'start': 'Amherst, MA', 'end': 'Hadley, MA'})
-    with mock.patch('controller.MapGraph.get_nodes') as mock_get_nodes:
-        mock_get_nodes.return_value = {
-            0: {'y': 42.3750369, 'x': -72.5195706},
-            1: {'y': 42.3494844, 'x': -72.5798426},
-            2: {'y': 42.3653504, 'x': -72.5430027}
-        }
-        with mock.patch('controller.controller.ox.shortest_path') as mock_shortest_path:
-            mock_shortest_path.return_value = [0, 1, 2]
-            lat_long_route = controller_obj.get_lat_long_route()
-            assert lat_long_route == [[42.3750369, -72.5195706], [42.3494844, -72.5798426], [42.3653504, -72.5430027]]
 
